5db345b996acfc31d369170e20e8a8c0cf6b1107
0sumrich <lynchardrich@gmail.com>
2021-07-20 13:33:37 +0100

    for got to awawit

--- api/data.js
+++ api/data.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg')
 module.exports = async (req, res) =>{
     try {
         const pool = new Pool()
-        const query = pool.query('select * from opening_hours;')
+        const query = await pool.query('select * from opening_hours;')
+        await pool.end()
         res.json(query.rows)
     } catch (e) {
         throw e
