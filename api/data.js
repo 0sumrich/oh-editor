9358985197c55189eae398a4853dae37b8dadce1
0sumrich <lynchardrich@gmail.com>
2021-07-20 13:28:01 +0100

    api change

--- api/data.js
+++ api/data.js
@@ -1,13 +1,12 @@
 const { Pool } = require('pg')
-const pool = new Pool()
 
 module.exports = async (req, res) =>{
     try {
+        const pool = new Pool()
         const query = pool.query('select * from opening_hours;')
-        res.json(res.rows)
+        await pool.end()
+        res.json(query.rows)
     } catch (e) {
         throw e
-   } finally {
-       await pool.end()
    }
 }
